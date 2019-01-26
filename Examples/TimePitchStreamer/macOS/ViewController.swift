8d2de2b480edc8972b46ef9604e4d3cebf250efc
Haris Ali <syedhali07@gmail.com>
2019-01-26 12:54:00 -0600

    Got mac app setup

---
+++ Examples/TimePitchStreamer/macOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimePitchStreamer
+//
+//  Created by Haris Ali on 1/26/19.
+//  Copyright © 2019 Ausome Apps LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
+
