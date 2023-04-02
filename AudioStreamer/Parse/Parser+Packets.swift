96ede7b83f825d6f9b8211367d865cd22deb320c
Haris Ali <syedhali07@gmail.com>
2023-04-02 11:38:15 -0400

    Use fixed packet callback method signature

--- AudioStreamer/Parse/Parser+Packets.swift
+++ AudioStreamer/Parse/Parser+Packets.swift
@@ -10,10 +10,9 @@ import Foundation
 import AVFoundation
 import os.log
 
-func ParserPacketCallback(_ context: UnsafeMutableRawPointer, _ byteCount: UInt32, _ packetCount: UInt32, _ data: UnsafeRawPointer, _ packetDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>) {
+func ParserPacketCallback(_ context: UnsafeMutableRawPointer, _ byteCount: UInt32, _ packetCount: UInt32, _ data: UnsafeRawPointer, _ packetDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>?) {
     let parser = Unmanaged<Parser>.fromOpaque(context).takeUnretainedValue()
-    let packetDescriptionsOrNil: UnsafeMutablePointer<AudioStreamPacketDescription>? = packetDescriptions
-    let isCompressed = packetDescriptionsOrNil != nil
+    let isCompressed = packetDescriptions != nil
     os_log("%@ - %d [bytes: %i, packets: %i, compressed: %@]", log: Parser.loggerPacketCallback, type: .debug, #function, #line, byteCount, packetCount, "\(isCompressed)")
     
     /// At this point we should definitely have a data format
@@ -22,7 +21,7 @@ func ParserPacketCallback(_ context: UnsafeMutableRawPointer, _ byteCount: UInt3
     }
     
     /// Iterate through the packets and store the data appropriately
-    if isCompressed {
+    if let packetDescriptions = packetDescriptions {
         for i in 0 ..< Int(packetCount) {
             let packetDescription = packetDescriptions[i]
             let packetStart = Int(packetDescription.mStartOffset)
